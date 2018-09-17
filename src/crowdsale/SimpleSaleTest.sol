bf0479905d448e76e535ec89cf0cf562b37a549b
kahmij <36455663+kahmij@users.noreply.github.com>
2018-09-17 13:17:07 +0200

    added KYC

--- src/crowdsale/SimpleSaleTest.sol
+++ src/crowdsale/SimpleSaleTest.sol
@@ -19,11 +19,24 @@ pragma solidity ^0.4.21;
 contract Ownable {
     address public owner;
 
+    /** WhiteList users and their phase status */
+    mapping(address => uint8) public whitelist;
+
+    /** operator address */
+    address public opsAddress;
+
     event OwnershipTransferred(
         address indexed previousOwner,
         address indexed newOwner
     );
+    event WhitelistUpdated(address indexed _account, uint8 _phase);
 
+
+    /** Modifier to check If the sender is the owner of the contract */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     /**
      * @dev The Ownable constructor sets the original `owner` of the contract to the sender
@@ -31,14 +44,6 @@ contract Ownable {
      */
     constructor() public {
         owner = msg.sender;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
     }
 
     /**
@@ -101,6 +106,7 @@ contract SimpleSaleTest is Ownable {
     // converts ETH to TOKEN and sends new TOKEN to the sender
     function contribute() external payable {
         require(msg.value>0);
+        require(whitelist[msg.sender] == 1);
         uint256 amount = msg.value * exchangeRate;
         totalSold += amount;
         ETHWallet.transfer(msg.value);
@@ -112,4 +118,89 @@ contract SimpleSaleTest is Ownable {
     function updateRate(uint256 rate) external onlyOwner {
         exchangeRate = rate;
     }
+
+    /** Internal Functions */
+
+    /**
+     *  @notice checks If the sender is the owner of the contract.
+     *
+     *  @param _address address to be checked if valid owner or not.
+     *
+     *  @return bool valid owner or not.
+     */
+    function isOwner(
+        address _address)
+        internal
+        view
+        returns (bool)
+    {
+        return (_address == owner);
+    }
+
+    /**
+     *  @notice check If the sender is the ops address.
+     *
+     *  @param _address address to be checked for ops.
+     *
+     *  @return bool valid ops or not.
+     */
+    function isOps(
+        address _address)
+        internal
+        view
+        returns (bool)
+    {
+        return (opsAddress != address(0) && _address == opsAddress);
+    }
+
+    /** External Functions */
+
+    /**
+     *  @notice Owner can change the verified operator address.
+     *
+     *  @param _opsAddress address to be set as ops.
+     *
+     *  @return bool address is successfully set as ops or not.
+     */
+    function setOpsAddress(
+        address _opsAddress)
+        external
+        onlyOwner
+        returns (bool)
+    {
+        require(_opsAddress != owner);
+        require(_opsAddress != address(this));
+        require(_opsAddress != address(0));
+
+        opsAddress = _opsAddress;
+
+        return true;
+    }
+
+    /**
+     *  @notice function to whitelist an address which can be called only by the ops address.
+     *
+     *  @param _account account address to be whitelisted
+     *  @param _phase 0: unwhitelisted, 1: whitelisted
+     *
+     *  @return bool address is successfully whitelisted/unwhitelisted.
+     */
+    function updateWhitelist(
+        address _account,
+        uint8 _phase)
+        returns (bool)
+    {
+        require(_account != address(0));
+        require(_phase <= 1);
+        require(isOps(msg.sender));
+
+        whitelist[_account] = _phase;
+
+        emit WhitelistUpdated(_account, _phase);
+
+        return true;
+    }
+
+
+
 }
