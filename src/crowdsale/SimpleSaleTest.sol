08a89f9b50ab7dbf114a850fb203e1ece56b3968
kahmij <36455663+kahmij@users.noreply.github.com>
2018-09-25 15:22:32 +0200

    outclass KYC contract

--- src/crowdsale/SimpleSaleTest.sol
+++ src/crowdsale/SimpleSaleTest.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.4.21;
 import "./Fiat.sol";
 import "./FiatMock.sol";
 import "./Ownable.sol";
+//import "./Whitelist.sol";
+
 /*
   BASIC ERC20 Sale Contract
   @author Hunter Long
@@ -12,14 +14,12 @@ import "./Ownable.sol";
 
 contract SimpleSaleTest is Ownable {
 
-    mapping(address => uint8) public whitelist;
-
-    address public opsAddress;
     uint256 public totalSold; //eurocents
 
     FiatContract public fiat;
     ERC20 public Token;
     address public ETHWallet;
+    Whitelist public white;
 
     uint256 public tokenSold;
     uint256 public tokenPrice;
@@ -29,15 +29,13 @@ contract SimpleSaleTest is Ownable {
 
     bool public crowdsaleClosed;
 
-
-    event WhitelistUpdated(address indexed _account, uint8 _phase);
     event Contribution(address from, uint256 amount);
 
-    constructor(address eth_wallet, address token_address) public {
+    constructor(address eth_wallet, address token_address, address whitelistcontract) public {
         ETHWallet = eth_wallet;
         Token = ERC20(token_address);
         crowdsaleClosed = false;
-
+        white = Whitelist(whitelistcontract);
         //need adjusted
         tokenSold = 0; //per contract
         tokenPrice = 20; //eurocents
@@ -50,7 +48,7 @@ contract SimpleSaleTest is Ownable {
 
     function () public payable {
         require(msg.value>0);
-        //require(whitelist[msg.sender] == 1);
+        require(white.isWhitelisted(msg.sender) == true);
         require(!crowdsaleClosed);
         require(now <= deadline && now >= start);
           //https://ethereum.stackexchange.com/questions/9256/float-not-allowed-in-solidity-vs-decimal-places-asked-for-token-contract
@@ -89,94 +87,19 @@ contract SimpleSaleTest is Ownable {
         return 200 * 100;
     }
 
-    /** Internal Functions */
-    /**
-     *  @notice checks If the sender is the owner of the contract.
-     *
-     *  @param _address address to be checked if valid owner or not.
-     *
-     *  @return bool valid owner or not.
-     */
-    function isOwner(
-        address _address)
-        internal
-        view
-        returns (bool)
-    {
-        return (_address == owner);
-    }
-    /**
-     *  @notice check If the sender is the ops address.
-     *
-     *  @param _address address to be checked for ops.
-     *
-     *  @return bool valid ops or not.
-     */
-    function isOps(
-        address _address)
-        internal
-        view
-        returns (bool)
-    {
-        return (opsAddress != address(0) && _address == opsAddress);
-    }
-
-    /** External Functions */
-
-    /**
-     *  @notice Owner can change the verified operator address.
-     *
-     *  @param _opsAddress address to be set as ops.
-     *
-     *  @return bool address is successfully set as ops or not.
-     */
-    function setOpsAddress(
-        address _opsAddress)
-        external
-        onlyOwner
-        returns (bool)
-    {
-        require(_opsAddress != owner);
-        require(_opsAddress != address(this));
-        require(_opsAddress != address(0));
-
-        opsAddress = _opsAddress;
-
-        return true;
-    }
     function closeCrowdsale() public onlyOwner returns (bool) {
         crowdsaleClosed = true;
         return true;
     }
 
-    /**
-     *  @notice function to whitelist an address which can be called only by the ops address.
-     *
-     *  @param _account account address to be whitelisted
-     *  @param _phase 0: unwhitelisted, 1: whitelisted
 
-     *
-     *  @return bool address is successfully whitelisted/unwhitelisted.
-     */
-    function updateWhitelist(
-        address _account,
-        uint8 _phase)
-        returns (bool)
-    {
-        require(_account != address(0));
-        require(_phase <= 1);
-        require(isOps(msg.sender));
-
-        whitelist[_account] = _phase;
-
-        emit WhitelistUpdated(_account, _phase);
-
-        return true;
-    }
 
 }
 
+contract Whitelist {
+    function isWhitelisted(address _account) constant returns (bool);
 
+}
 
 contract ERC20 {
     uint public totalSupply;
