7e7d2548218dbfb31c91201f2c93392a4c068f39
kahmij <36455663+kahmij@users.noreply.github.com>
2018-09-29 12:45:00 +0200

    revert changes

--- src/crowdsale/Whitelist.sol
+++ src/crowdsale/Whitelist.sol
@@ -1,112 +1,133 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.25;
 
-import "./Fiat.sol";
-import "./FiatMock.sol";
-import "./Ownable.sol";
-//import "./Whitelist.sol";
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
 
-/*
-  BASIC ERC20 Sale Contract
-  @author Hunter Long
-  @repo https://github.com/hunterlong/ethereum-ico-contract
-  (c) Max / SCU GmbH 2018. The MIT Licence.
-*/
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-contract SimpleSaleTest is Ownable {
-
-    FiatContract public fiat;
-    ERC20 public Token;
-    address public ETHWallet;
-    Whitelist public white;
-
-    uint256 public tokenSold;
-    uint256 public tokenPrice;
-
-    uint256 public deadline;
-    uint256 public start;
-
-    bool public crowdsaleClosed;
-
-    event Contribution(address from, uint256 amount);
-    event TokenSold(address indexed customer, uint256 amount);
-
-    constructor(address eth_wallet, address token_address, address whitelistcontract) public {
-        ETHWallet = eth_wallet;
-        Token = ERC20(token_address);
-        crowdsaleClosed = false;
-        white = Whitelist(whitelistcontract);
-        //need adjusted
-        tokenSold = 0; //per contract
-        tokenPrice = 20; //eurocents
-        fiat = FiatContract(0x2CDe56E5c8235D6360CCbb0c57Ce248Ca9C80909);  // Rinkeby and in-memory
-        //https://ethereum.stackexchange.com/questions/34110/compare-dates-in-solidity
-        deadline = now + 110 * 1 days;
-        start = now; //+ 110 * 1 days;
-
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        owner = msg.sender;
     }
 
-    function () public payable {
-        require(msg.value>0);
-        require(white.isWhitelisted(msg.sender) == true);
-        require(!crowdsaleClosed);
-        require(now <= deadline && now >= start);
-          //https://ethereum.stackexchange.com/questions/9256/float-not-allowed-in-solidity-vs-decimal-places-asked-for-token-contract
-          //fee falls away
-
-          uint256 amount = (msg.value / getTokenPrice()) * 1 ether;
-
-          //afterwards calculate  pre sale bonusprogramm
-          if(tokenSold < 6000000)
-          {
-              amount = amount + ((amount * 25) / 100);
-          }
-          else if(tokenSold < 12000000)
-          {
-              amount = amount + ((amount * 15) / 100);
-          }
-          else
-          {
-              amount = amount + ((amount * 10) / 100);
-          }
-
-          tokenSold = tokenSold + amount;
-          emit TokenSold(msg.sender, amount);
-
-          ETHWallet.transfer(msg.value);
-          Token.transferFrom(owner, msg.sender, amount);
-          emit Contribution(msg.sender, amount);
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
 
-
-    function getTokenPrice() internal view returns (uint256) {
-        return getEtherInEuroCents() * tokenPrice / 100;
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param _newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
     }
-
-    function getEtherInEuroCents() internal view returns (uint256) {
-        return fiat.EUR(0) * 100;
-
-    }
-
-    function closeCrowdsale() public onlyOwner returns (bool) {
-        crowdsaleClosed = true;
-        return true;
-    }
-
-
-
 }
 
-contract Whitelist {
-    function isWhitelisted(address _account) constant returns (bool);
+contract Whitelist is Ownable {
 
+  address public opsAddress;
+  mapping(address => uint8) public whitelist;
+
+  event WhitelistUpdated(address indexed _account, uint8 _phase);
+
+  function isWhitelisted(address _account) public constant returns (bool) {
+      return whitelist[_account] == 1;
+  }
+
+  /**
+ *  @notice function to whitelist an address which can be called only by the ops address.
+ *
+ *  @param _account account address to be whitelisted
+ *  @param _phase 0: unwhitelisted, 1: whitelisted
+
+ *
+ *  @return bool address is successfully whitelisted/unwhitelisted.
+ */
+function updateWhitelist(
+    address _account,
+    uint8 _phase) public
+    returns (bool)
+{
+    require(_account != address(0));
+    require(_phase <= 1);
+    require(isOps(msg.sender));
+
+    whitelist[_account] = _phase;
+
+    emit WhitelistUpdated(_account, _phase);
+
+    return true;
 }
 
-contract ERC20 {
-    uint public totalSupply;
-    function balanceOf(address who) public constant returns (uint);
-    function allowance(address owner, address spender) public constant returns (uint);
-    function transfer(address to, uint value) public returns (bool ok);
-    function transferFrom(address from, address to, uint value) public returns (bool ok);
-    function approve(address spender, uint value) public returns (bool ok);
-    function mint(address to, uint256 value) public returns (uint256);
+
+  /** Internal Functions */
+  /**
+   *  @notice checks If the sender is the owner of the contract.
+   *
+   *  @param _address address to be checked if valid owner or not.
+   *
+   *  @return bool valid owner or not.
+   */
+  function isOwner(
+      address _address)
+      internal
+      view
+      returns (bool)
+  {
+      return (_address == owner);
+  }
+  /**
+   *  @notice check If the sender is the ops address.
+   *
+   *  @param _address address to be checked for ops.
+   *
+   *  @return bool valid ops or not.
+   */
+  function isOps(
+      address _address)
+      internal
+      view
+      returns (bool)
+  {
+      return (opsAddress != address(0) && _address == opsAddress) || isOwner(_address);
+  }
+
+  /** External Functions */
+
+  /**
+   *  @notice Owner can change the verified operator address.
+   *
+   *  @param _opsAddress address to be set as ops.
+   *
+   *  @return bool address is successfully set as ops or not.
+   */
+  function setOpsAddress(
+      address _opsAddress)
+      external
+      onlyOwner
+      returns (bool)
+  {
+      require(_opsAddress != owner);
+      require(_opsAddress != address(this));
+      require(_opsAddress != address(0));
+
+      opsAddress = _opsAddress;
+
+      return true;
+  }
+
 }
